cbc0494905d65bd8a137220ccac0dad68cfbd07b
oscar <oscareucedaf1@gmail.com>
2026-02-23 01:22:42 -0500

    added initialization code for firebase

--- Korah (Beta)/Views/ToDo/Timer/KorahApp.swift
+++ Korah (Beta)/Views/ToDo/Timer/KorahApp.swift
@@ -1,9 +1,22 @@
 import SwiftUI
 import Foundation
 import UserNotifications
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct KorahApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     init() {
         setupNotifications()
     }
